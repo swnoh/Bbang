3a0af7fcd38b9745aeffe54b01f0a1c9f8a44ed3
Philip <hohohsh@icloud.com>
2018-04-18 21:23:52 -0400

    Adding Description of product

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,23 +3,56 @@ import SideBar from "./SideBar";
 import "./Product.css";
 import Swiper from "react-id-swiper";
 import style from "react-id-swiper/src/styles/css/swiper.css";
+import ReactDrawer from "react-drawer";
+import "react-drawer/lib/react-drawer.css";
 
 class Product extends Component {
   constructor(props) {
     super(props);
+    this.state = { open: false, position: "right", noOverlay: false, side_desc: ["Individual Size", "Perfect for brunch", "Bulgogi is Korean Traditional beef dish which is marinated in sweet soy sauce", "Broccoli, Cheese, Tomato, Bulgogi(beef)"] };
+    this.toggleDrawer = this.toggleDrawer.bind(this);
+    this.closeDrawer = this.closeDrawer.bind(this);
+    this.onDrawerClose = this.onDrawerClose.bind(this);
+    this.setNoOverlay = this.setNoOverlay.bind(this);
     this.handleClick = this.handleClick.bind(this);
-    this.slideTo = this.slideTo.bind(this);
-    this.swiper = null;
   }
 
-  slideTo(idx){
+
+  setNoOverlay(e) {
+    this.setState({ noOverlay: true });
+  }
+  toggleDrawer() {
+    this.setState({ open: !this.state.open });
+  }
+  closeDrawer() {
+    this.setState({ open: false });
+  }
+  onDrawerClose() {
+    this.setState({ open: false });
+  }
+
+  slideTo(idx) {
     this.swiper.slideTo(idx);
   }
 
   handleClick(e) {
     e.preventDefault();
     this.slideTo(e.currentTarget.id);
+    this.setState({
+      side_desc: this.props.products[e.currentTarget.id].description
+    });
+    console.log(this.state.side_desc);
   }
+
+  toggle = () => {
+    let { toggle } = this.state;
+
+    this.setState({ toggle: !toggle });
+  };
+
+  logState = () => {
+    console.log(`Drawer now ${this.state.open ? "open" : "closed"}`);
+  };
 
   render() {
     const params = {
@@ -31,23 +64,38 @@ class Product extends Component {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev"
       },
-      autoHeight: true
+      autoHeight: true,
+      autoWidth: true
     };
+    
     const { products } = this.props;
+    const side_description = this.state.side_desc.map((r, idx) =>
+                  <li key={idx}> {r} </li> );
     return (
       <div className="product">
         <div id="sideBar_container">
-          <SideBar products={products} onClicked={this.handleClick} value={products.id}  />
+          <SideBar products={products} onClicked={this.handleClick} value={products.id} />
         </div>
         <div id="swiper_container">
-          <Swiper {...params} ref={node => this.swiper = node !== null ? node.swiper : null }>
-            {this.props.products.map((r, index) => (
-              <div>
+          <Swiper {...params} ref={node => (this.swiper = node !== null ? node.swiper : null)}>
+            {this.props.products.map((r, index) => <div>
                 <img src={r.imagePath} alt={r.title} id="swiperImg" />
-              </div>
-            ))}
+              </div>)}
           </Swiper>
         </div>
+        <div id="drawer_container">
+          <button onClick={this.toggleDrawer} disabled={this.state.open && !this.state.noOverlay}>
+            Test
+          </button>
+        </div>
+        <ReactDrawer open={this.state.open} position={this.state.position} onClose={this.onDrawerClose} noOverlay={this.state.noOverlay}>
+          <div class="product_description">
+            <i onClick={this.closeDrawer} className="icono-cross" />
+            <ul>
+              {side_description}
+            </ul>
+          </div>
+        </ReactDrawer>
       </div>
     );
   }
