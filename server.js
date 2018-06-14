aad9b36e29fbdbbd34d2816f35adc701278e9b69
Seungwan Noh <seungwan.noh@gmail.com>
2018-06-14 03:48:05 -0400

    update server port number

--- server.js
+++ server.js
@@ -58,10 +58,6 @@ server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/build/index.html'));
 });
 
-server.get("/test", function(req, res) {
-  res.send(`This is the Server Page! ${PORT}`);
-});
-
 server.listen(PORT, () =>
   console.log(`GraphQL Server is now running on http://localhost:${PORT}`)
 );
